69cfdb22c780d72b8321efe2eeedae015b99ddcf
snail-panda <jin128@protonmail.com>
2025-06-09 21:43:55 -0400

    console check

--- ranking/2025/spring/week09/main.js
+++ ranking/2025/spring/week09/main.js
@@ -63,6 +63,7 @@ fetch('ranking-week09-spring2025.json')
       if (trendLabel && trendIcon) {
         const label = entryData.trend.label.toLowerCase(); // 'UP' → 'up'
         trendLabel.textContent = entryData.trend.label;
+        console.log(`images/trends/${label}-arrow.png`);
         trendIcon.src = `images/trends/${label}-arrow.png`;
         trendIcon.className = `trend-icon-${label}`;
       }
