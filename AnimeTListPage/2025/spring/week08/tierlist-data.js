c6805703faca79fac0a28aa1fe8f7190099f945b
snail-panda <jin128@protonmail.com>
2025-06-13 22:50:13 -0400

    tierlist page week7 update

--- AnimeTListPage/2025/spring/week08/tierlist-data.js
+++ AnimeTListPage/2025/spring/week08/tierlist-data.js
@@ -1,8 +1,9 @@
 const tierlistData = {
   seasonTitle: "2025 Spring Anime Mid-Season Tier List – Episodes 7–8",
   seasonDate: "May 31, 2025",
-  intro: "We’re already halfway through the season! Here’s my updated tier list after Episode 8.",
-  conclusion: "That wraps up my Ep. 7–8 impressions. Thanks for reading!",
+  intro: "We’re already halfway through the season! Here’s my updated tier list after Episode 8. Some shows continue to shine, others fell off, and a few... I had to drop.",
+  content: "The mid-season tier highlight definitely revolves around the sub-top tiers like A, B, and C. While SS, S, and D are pretty much locked in, mid-tier titles such as Mobile Suit Gundam GQuuuuuuX, Kowloon Generic Romance, and From the Old Country Bumpkin... are still holding some potential to rise — though honestly, they're quite wobbly. From my personal view, Gundam and From the Old... have finally started showing some of their own charm, though not fully baked yet. Hopefully they'll offer a bit more than what we saw back in Episode 1. \nThe mid-season highlight tends to revolve around the sub-top tiers — A, B, and C. While SS, S, and D are mostly stable, mid-tier titles often show both signs of potential and inconsistency. Some shows, like Mobile Suit Gundam GQuuuuuuX, Kowloon Generic Romance, and From the Old Country Bumpkin..., are starting to reveal their strengths, but still feel a bit shaky overall. Hopefully, the coming episodes will give a clearer sense of where these shows are really headed. \nMid-season shifts mainly appear in the mid-tiers — A, B, and C — where some titles keep wobbling while showing small flashes of potential. For now, shows like Mobile Suit Gundam GQuuuuuuX, Kowloon Generic Romance, and From the Old Country Bumpkin... are still hanging in there, slightly improving but not fully convincing yet. I hope the next few episodes will give them more room to settle and show stronger direction.",
+  conclusion: "That wraps up my Ep. 7–8 impressions. Thanks for reading! I'll see you again for the 2025 Spring Final Tier List.  (And hey—don't forget to let me know where your favorites landed!)",
   tierImage: "https://i.postimg.cc/CK5DYTZ7/2025midspring-tier-list-ep7-8.png",
   tierGuide: `
     <h4 class="criteria-title">Tier Guide</h4>
@@ -17,17 +18,62 @@ const tierlistData = {
       <li><span class="na">⚫ N/A – Disqualified</span> Judgement made. In the void.</li>
     </ul>
   `,
-  disclaimer: `<strong>*Note on N/A Disqualified Tier:</strong><br>Most titles in the N/A Disqualified tier were dropped or excluded from my watch pool after the first few episodes or even during it. Some didn’t last more than a few minutes, while others were ruled out at a glance—or not even. That doesn’t mean they’re all “absolute trash.” Most just weren’t my cup of tea or didn’t suit the ranking format. Some shows—even if good or fun in their own way—just don’t fit the structure or vibe of my weekly list. That said, yeah, a few were truly dookie.`,
+  disclaimer: `
+  <div class="disclaimer-title">*Note on N/A Disqualified Tier:</div>
+  Most titles in the N/A Disqualified tier were dropped or excluded from my watch pool after the first few episodes or even during it. Some didn’t last more than a few minutes, while others were ruled out at a glance—or not even. That doesn’t mean they’re all “absolute trash.” Most just weren’t my cup of tea or didn’t suit the ranking format. Some shows—even if good or fun in their own way—just don’t fit the structure or vibe of my weekly list. That said, yeah, a few were truly dookie.`,
   
   tiers: {
-    "SS": [],
-    "S": [],
-    "A": [],
-    "B": [],
-    "C": [],
-    "D": [],
-    "Dropped": [],
-    "NA": [],
-    "RecentlyDropped": []
+     "SS": [
+      "The Apothecary Diaries 2nd Season",
+      "Sword of the Demon Hunter",
+      "Shoshimin: How to Become Ordinary 2"
+    ],
+    "S": [
+      "Apocalypse Hotel",
+      "A Ninja and an Assassin Under One Roof",
+      "Me and the Alien MuMu"
+    ],
+    "A": [
+      "Mobile Suit Gundam GQuuuuuuX",
+      "The Catcher in the Ballpark!"
+    ],
+    "B": [
+      "Please Put Them On",
+      "Kowloon Generic Romance"
+    ],
+    "C": [
+      "From Old Country Bumpkin to Master Swordsman: My Hotshot Disciples Are All Grown Up Now",
+      "Your Forma",
+      "Miru: Paths to My Future"
+    ],
+    "D": [
+      "Can a Boy-Girl Friendship Survive?",
+      "Once Upon a Witch's Death"
+    ],
+    "Dropped": [
+      "LAZARUS",
+      "Uma Musume: Cinderella Grey",
+      "The Dinner Table Detective",
+      "Mono",
+      "Anne Shirley",
+      "Zatsu Tabi: That's Journey",
+      "Rock wa Lady no Tashinami deshite",
+      "Food For the Soul",
+      "The Shiunji Family Children",
+      "The Too-Perfect Saint: Tossed Aside by My Fiancé and Sold to Another Kingdom",
+      "Summer Pockets"
+    ],
+    "N/A": [
+      "The Mononoke Lecture Logs of Chuzenji-sensei",
+      "The Brilliant Healer's New Life in the Shadows",
+      "Maebashi Witches",
+      "Aharen Is Indecipherable 2nd Season",
+      "Moonrise",
+      "Yandere Dark Elf: She Chased Me All the Way From Another World!",
+      "The Beginning After the End",
+      "Witch Watch"
+],
+    "RecentlyDropped": ["Your Forma", "Uma Musume: Cinderella Grey", "Summer Pockets", "LAZARUS"
+]
   }
 }
