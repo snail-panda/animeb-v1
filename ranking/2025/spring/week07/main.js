ac819122176542f666d6d8f4405d031c8e12cef7
snail-panda <jin128@protonmail.com>
2025-06-16 15:17:53 -0400

    wrp detail removed

--- ranking/2025/spring/week07/main.js
+++ ranking/2025/spring/week07/main.js
@@ -79,16 +79,13 @@ fetch('ranking-week07-spring2025.json')
         trendIcon.className = `trend-icon-${label}`;
       }
 
-     
-  // WRP Breakdown埋め込み
-     const wrpDetailBtn = el.querySelector('.wrp-detail-btn');
-if (wrpDetailBtn && entryData.wrp_breakdown) {
-  const breakdown = Object.entries(entryData.wrp_breakdown)
-    .map(([key, val]) => `${capitalize(key)}: ${val}`)
-    .join(', ');
-  wrpDetailBtn.dataset.breakdown = breakdown;
-}
+      // WRPスコア更新
+      const wrpScoreEl = el.querySelector('.wrp-score');
+      if (wrpScoreEl) {
+        wrpScoreEl.innerHTML = `${entryData.wrp_score}<span class="wrp-score-unit">pt</span>`;
+      }
 
+ 
       // Totalスコア更新
       const scoreEl = el.querySelector('.score');
       if (scoreEl) {
