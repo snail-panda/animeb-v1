eb753878f81876971a031448663285954665367e
snail-panda <jin128@protonmail.com>
2025-06-19 21:46:17 -0400

    Update main.js

--- ranking/2025/spring/week01/main.js
+++ ranking/2025/spring/week01/main.js
@@ -67,15 +67,13 @@ if (jpTitleEl) {
 
       // Review挿入処理
         const reviewTag = titleEl.querySelector('.review-tag');
-        if (entryData.review && (entryData.review.en || entryData.review.jp)) {
-  reviewTag.dataset.reviewEn = entryData.review.en || "";
-  reviewTag.dataset.reviewJp = entryData.review.jp || "";
-  reviewTag.style.display = 'inline-block';
-} else {
-  reviewTag.style.display = 'none';
-}
+        if (entryData.review && entryData.review.trim() !== '') {
+          reviewTag.dataset.review = entryData.review;
+          reviewTag.style.display = 'inline-block';
+        } else {
+          reviewTag.style.display = 'none';
+        }
 
-  
 
       // トレンド情報更新
       const trendLabel = el.querySelector('.trend-label');
