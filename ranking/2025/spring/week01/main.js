407a27fd798ec1aa5c29c0a74abdcba7f3156444
snail-panda <jin128@protonmail.com>
2025-07-02 04:17:02 -0400

    Update main.js

--- ranking/2025/spring/week01/main.js
+++ ranking/2025/spring/week01/main.js
@@ -38,7 +38,7 @@ function adjustScoreBars() {
 }
 
 // ========== JSON読み込み & DOM更新 ==========
-fetch('ranking-week02-spring2025.json')
+fetch('ranking-week01-spring2025.json')
   .then(response => response.json())
   .then(data => {
     // メタ情報更新
