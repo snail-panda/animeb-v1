daf60ba796042da555cd525df38adb2e8a5b068d
snail-panda <jin128@protonmail.com>
2025-07-21 16:36:48 -0400

    viewing status js comment out

--- assets/js/ranking/weekly/main.js
+++ assets/js/ranking/weekly/main.js
@@ -774,10 +774,11 @@ document.querySelectorAll('.detail-toggle').forEach(el => {
 });
 
 // VIEWING STATUS
-
+/*
 const toggleBtn = document.getElementById('toggleBtn');
     const details = document.getElementById('details');
     toggleBtn.addEventListener('click', () => {
       const expanded = details.classList.toggle('active');
       toggleBtn.textContent = expanded ? '▲' : '▼';
     });
+*/
