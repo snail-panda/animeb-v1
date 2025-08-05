a256c598e45d4f2694eccc58656b8c71267186ae
snail-panda <jin128@protonmail.com>
2025-08-04 23:50:42 -0400

    week12 and 1 for summer

---
+++ assets/js/tierlist/weekly/tierguide-week12.js
@@ -0,0 +1,19 @@
+const tierGuideData = {
+  tierGuide: `
+    <h4 class="criteria-title">Tier Guide</h4>
+    <ul class="criteria-list">
+	  <li><span class="sss">🟣 SSS – TRANSCENDENT/GOD-like tier</span> This is the anime. Beyond one’s personal taste, how can one not watch this? I know some still don’t. But that would be an outrageous mistake. </li>
+      <li><span class="ss">🔴 SS – CELESTIAL</span> Crème de la crème. Absolute must-watch. You'll be absolutelly grateful.</li>
+      <li><span class="s">🟠 S – GREAT</span> Strong execution, great energy,and consistency last til the end. Even if it's not your cup of tea, you won't regret it. This is where recommendations really start.</li>
+      <li><span class="a">🟡 A – GOOD</span> Solid work, consistently enjoyable and featuring some great episodes, though certain parts may not have matched the overall potential. Fluctuations in pacing or focus, and occasional unsatisfying developments, keep it from achieving the highest ranks. Still, a confidently good choice.</li>
+      <li><span class="b">⚪ B – DECENT</span> Not essential, but fully watchable. Perfectly fine for light watching. You won't miss much if you skip it.</li>
+      <li><span class="c">🟢 C – NOT (REALLY) BAD / ABYSMAL</span> Mostly fine for killing time, with occasional enjoyable episodes or shining moments. You may not really regret watching it, but overall it’s not deeply satisfying, either. Better suited for those who know exactly what and why they are watching — fans who fully accept niche or even absurd premises as part of their anime joy.</li>
+      <li><span class="d">🔻 D – TRASH</span> Pure and barely watchable trash that has lost (almost) any sense of coherence, quality, or anything that matters to anime, but can still offer a bizarre kind of fun if you completely shut your brain off and let the nonsense wash over you. Perfect as a ridiculous nightcap when you’re too tired to care.
+      <li><span class="dropped">🔵 DROPPED / PICKING BACK UP</span> These titles entered the ranking pool after at least one episode and sometimes lasted deep into the season — but ultimately were dropped for reasons like unbearable quality, personal incompatibility, or simply no longer holding my interest. Although not fully excluded and always open to a second look (especially if later buzz suggests big improvements), most of these shows are unlikely to return. And if they do, they often risk getting dropped again after another quick check-in. That’s the nature of the “dropped” zone — ranging from instant quits to near-season finales, they just didn’t make the final cut.</li>
+      <li><span class="na">⚫ N/A – Disqualified</span> Judgement made. In the void.</li>
+    </ul>
+  `,
+  disclaimer: `
+  <div class="disclaimer-title">*Note on N/A Disqualified Tier:</div>
+  Most titles in the N/A Disqualified tier were dropped or excluded from my watch pool after the first few episodes or even during it. Some didn’t last more than a few minutes, while others were ruled out at a glance—or not even. That doesn’t mean they’re all “absolute trash.” Most just weren’t my cup of tea or didn’t suit the ranking format. Some shows—even if good or fun in their own way—just don’t fit the structure or vibe of my weekly list. That said, yeah, a few were truly dookie.`
+};
