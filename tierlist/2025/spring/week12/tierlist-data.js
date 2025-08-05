b039640f7172d71dc9cc8afa85d22545530da436
snail-panda <jin128@protonmail.com>
2025-08-05 00:42:57 -0400

    spring week12 update

--- tierlist/2025/spring/week12/tierlist-data.js
+++ tierlist/2025/spring/week12/tierlist-data.js
@@ -1,10 +1,4 @@
-const tierlistData = {
-  seasonTitle: "2025 Spring Anime Season-Final Tier List – Episodes 12–13",
-  seasonDate: "July 03, 2025",
-  intro: "The 2025 Spring anime season has officially come to its end, and with that, here’s the final Tier List — covering not just the shows ranked during the season, but also every title fully watched all the way through. Some series shook things up in the latter half, while others coasted along — so where did they all land in the end? Let’s dive in and find out!",
-  content: "<p>The 2025 Spring anime season was clearly headlined by <strong>The Apothecary Diaries Second Season</strong> (<em>Kusuriya no Hitorigoto 2nd Season</em>) and <strong>Shoshimin: How to Become Ordinary 2</strong> (<em>Shoshimin Series 2nd Season</em>), which kept their top spots all the way through with no real stumbles—not even a tiny—at all. Following closely behind, <strong>Sword of the Demon Hunter</strong> (absolutely fantastic job!! Yokohama Animation Lab!!!) and <strong>Apocalypse Hotel</strong> stood out as powerful and incredible surprises that almost caught up to the SSS-tier legends at certain points — major props there. In particular, <strong>Sword of the Demon Hunter</strong> has another season ahead, and I’m excited to see how it continues — hopefully without dropping tiers!</p> <p>Meanwhile, <strong>The Catcher in the Ballpark</strong> (<em>Ballpark de Tsukamaete!</em>) deserves a big shout-out. Even without a massive budget or a high-profile spot, it delivered steady, heartfelt quality across the entire cour. Consistency like that is no small feat. Hats off to them. Moving on to the mid-tier zone, <strong>A Ninja and an Assassin Under One Roof</strong> (<em>Ninja to Koroshiya no Futari Gurashi</em>) and <strong>Me and the Alien MUUMUU</strong> (<em>Uchujin Muumuu</em>) absolutely deserve a mention for delivering some truly standout 10/11-point episodes through the first half and into the early late-cour. Honestly, they were holding S-tier status for a while and even looked like they might push higher, before losing steam near the end.</p> <p><strong>Kowloon Generic Romance</strong>, on the other hand, opened with fantastic atmosphere and mystery but couldn’t stick the landing, mostly due to an overly cramped one-cour adaptation of an unfinished source. As for <strong>Mobile Suit Gundam GQuuuuuuX</strong> (<em>Kidou Senshi Gundam GQuuuuuuX</em>), let’s be real — it was best enjoyed by turning your brain off and just rolling with its ridiculous chaos, which made for plenty of heated debate online.</p><p> Down in the D-tier, we had the glorious trainwrecks: <strong>Once Upon a Witch's Death</strong> (<em>Aru Majo ga Made Shinu Made</em>), <strong>Summer Pockets</strong>, and <strong>Can a Boy-Girl Friendship Survive?</strong> (<em>Danjo no Yuujou wa Seiritsu Suru? Iya, Shinai!!</em>). These shows were complete structural disasters and absurd and ridiculous in every way, but honestly, they were so bad they circled back around to being entertaining in their own bizarre ways, yeah, it was fun if nothing else. To be honest, there were plenty of shows (like <strong>Mono</strong>, <strong>Zatsu Tabi: That's Journey</strong>, <strong>The Shiunji Family Children</strong> and so on) I ended up dropping without regret (simply because I couldn't stand), but hey, that’s part of the seasonal ride too. I can’t forget to mention <strong>Lazarus</strong>, either — it delivered such a jaw-dropping first episode I gave it sky-high expectations, only for it to nosedive right after and end up dropped. That was a real heartbreak.</p><p> In the end, whether you stuck to the top-tier masterpieces or dipped your toes into the more questionable titles or trashy side of the catalog, I hope you found something that made you smile.</p>",
-  conclusion: "And that wraps up our final tier list for the 2025 Spring anime season. Honestly, it felt like the weeks just flew by — there were plenty of truly amazing shows, some surprises (in both ways), and a couple of (hopefully) “what-was-that-even” moments along the way. Starting by checking close to 40 to 50 titles and ending up fully watching over 20 titles including unranked ones was no joke (yeah, it was a wild but memorable ride). Overall, I hope you enjoyed the season as much as I did.<br><br>Summer 2025 is already right around the corner, and I’m looking forward to catching up with you again for the first tier list of the Summer season and the weekly rankings. As always, feel free to let me know where your personal top and worst picks landed, or any other thoughts you have! See you next season!",
-  tierImage: "../../../../images/tier-lists/2025/spring/tierlist-2025-spring-week12.png",
+const tierGuideData = {
   tierGuide: `
     <h4 class="criteria-title">Tier Guide</h4>
     <ul class="criteria-list">
@@ -21,77 +15,5 @@ const tierlistData = {
   `,
   disclaimer: `
   <div class="disclaimer-title">*Note on N/A Disqualified Tier:</div>
-  Most titles in the N/A Disqualified tier were dropped or excluded from my watch pool after the first few episodes or even during it. Some didn’t last more than a few minutes, while others were ruled out at a glance—or not even. That doesn’t mean they’re all “absolute trash.” Most just weren’t my cup of tea or didn’t suit the ranking format. Some shows—even if good or fun in their own way—just don’t fit the structure or vibe of my weekly list. That said, yeah, a few were truly dookie.`,
-  
-  tiers: {
-     "SSS": [
-	  "The Apothecary Diaries 2nd Season",
-	  "Shoshimin: How to Become Ordinary 2"
-	],  
-    "SS": [
-      "Sword of the Demon Hunter",
-      "Apocalypse Hotel"
-    ],
-    "S": [
-      "The Catcher in the Ballpark!"
-    ],
-    "A": [
-      "A Ninja and an Assassin Under One Roof",
-      "Me and the Alien MuMu"
-    ],
-    "B": [
-      "Mobile Suit Gundam GQuuuuuuX",
-      "Witch Watch"
-    ],
-    "C": [
-      "Please Put Them On, Takamine-san!",
-      "Kowloon Generic Romance",
-      "From Old Country Bumpkin to Master Swordsman: My Hotshot Disciples Are All Grown Up Now, and They Won’t Leave Me Alone",
-      "Uma Musume: Cinderella Grey",
-      "I’m the Evil Lord of an Intergalactic Empire!",
-      "Miru: Paths to My Future"
-    ],
-    "D": [
-      "Can a Boy-Girl Friendship Survive?",
-      "Once Upon a Witch's Death",
-      "Summer Pockets",
-      "The Unaware Atelier Master",
-      "The Beginning After the End",
-      "The Brilliant Healer's New Life in the Shadows",
-      "Lycoris Recoil: Friends are thieves of time"
-    ],
-    "Dropped": [
-      "LAZARUS",
-      "The Dinner Table Detective",
-      "Mono",
-      "Anne Shirley",
-      "Zatsu Tabi: That's Journey",
-      "Rock wa Lady no Tashinami deshite",
-      "Food For the Soul",
-      "The Shiunji Family Children",
-      "The Too-Perfect Saint: Tossed Aside by My Fiancé and Sold to Another Kingdom",
-      "The Gorilla God’s Go-To Girl",
-      "Your Forma",
-      "Maebashi Witches"
-    ],
-    "N/A": [
-      "The Mononoke Lecture Logs of Chuzenji-sensei",
-      "Yandere Dark Elf: She Chased Me All the Way From Another World!",
-      "Moonrise",
-      "Aharen Is Indecipherable 2nd Season",
-      "Moonrise",
-      "Yandere Dark Elf: She Chased Me All the Way From Another World!",
-      "Makina-san's a Love Bot?!",
-      "#Compass2.0: Combat Providence Analysis System",
-      "Go! Go! Loser Ranger! 2nd season",
-      "I’ve Been Killing Slimes for 300 Years and Maxed Out My Level 2nd Season",
-      "YAIBA: Samurai Legend",
-      "Bye Bye, Earth 2nd Season",
-      "Teogonia"
-    ],
-    "RecentlyDropped": [
-      "Your Forma",
-      "LAZARUS"
-]
-  }
-}
+  Most titles in the N/A Disqualified tier were dropped or excluded from my watch pool after the first few episodes or even during it. Some didn’t last more than a few minutes, while others were ruled out at a glance—or not even. That doesn’t mean they’re all “absolute trash.” Most just weren’t my cup of tea or didn’t suit the ranking format. Some shows—even if good or fun in their own way—just don’t fit the structure or vibe of my weekly list. That said, yeah, a few were truly dookie.`
+};
